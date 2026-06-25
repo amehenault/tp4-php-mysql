ed62a6527475c45e35bc0084588172351abca588
root <root@Piou-piou-computer.localdomain>
2026-06-25 10:04:34 -0400

    Suppression du fichier database.sql inutile

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `tasks` (
-  `id` INT AUTO_INCREMENT PRIMARY KEY,
-  `title` VARCHAR(255) NOT NULL,
-  `description` TEXT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
-INSERT INTO `tasks` (`title`, `description`) VALUES 
-('Acheter un chien', 'Choisir un harnais et une médaille'),
-('Lire le livre Mon deuxième cerveau', 'Déployer les notes sur Obsidian');
